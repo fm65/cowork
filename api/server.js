417744aa20c7d9cee71651b6c0e4c92dd096e214
Jean-Luc LYS <jluc@lys-phorimavong.com>
2020-08-28 11:41:30 +0200

    create api project

---
+++ api/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const models = require('./models');
+const routes = require('./routes');
+
+async function bootstrap() {
+    await models.sequelize.authenticate();
+    await models.sequelize.sync();
+
+    const app = express();
+
+    // Add headers
+    app.use(function (req, res, next) {
+
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+        // Pass to next layer of middleware
+        next();
+    });
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    routes(app);
+    app.listen(process.env.PORT, () => console.log(
+        `Listening on http://localhost:${
+            process.env.PORT}/ (Press CTRL+C to quit)`));
+}
+
+
+bootstrap();
---
+++ api/utils/jwt.utils.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+    /**
+     * @param userId  {integer}
+     * @param mode    {boolean}
+     * @returns       {string}
+     */
+    generateTokenForUser: function(userData){
+        return jwt.sign({
+                id        : userData.id,
+                firstName : userData.firstName,
+                lastName  : userData.lastName,
+                email     : userData.email,
+                role      : userData.role
+            },
+            process.env.TOKEN_SECRET_KEY,
+            {
+                expiresIn: '1d'
+            }
+        )
+    }
+}
